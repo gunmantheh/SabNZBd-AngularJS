3c752e36d543f5f42e2f8ab9efed19e2c105046d
GunmanTheH <4093289+gunmantheh@users.noreply.github.com>
2015-03-16 14:18:02 +0100

    Beatified the code (atom-beautify)

--- templates/static/main.js
+++ templates/static/main.js
@@ -1,51 +1,73 @@
 var app = angular.module("sabnzbdApp", ['ui.bootstrap', 'ngAnimate']);
-app.controller("PostsCtrl", function($scope, $http, $timeout, queueRefresh)
-{
-$scope.refresh = 5;  
+app.controller("PostsCtrl", function($scope, $http, $timeout, queueRefresh) {
+  $scope.refresh = 5;
 
-$scope.deleteNZB = function(id, event)
-{
-	console.log("Removing queue item", id);
-	$http.get('tapi', {params: { mode:"queue", name:"delete", value:id, output:"json", apikey: "$session" }})
-};
+  $scope.deleteNZB = function(id, event) {
+    console.log("Removing queue item", id);
+    $http.get('tapi', {
+      params: {
+        mode: "queue",
+        name: "delete",
+        value: id,
+        output: "json",
+        apikey: "$session"
+      }
+    })
+  };
 
-$scope.changeCategory = function(id, category, event)
-{
-	console.log("Changing category ", id, category);
-	$http.get('tapi', {params: { mode:"change_cat", value:id, value2:category, output:"json", apikey: "$session" }}).success(function(data, status, headers, config)
-	{
-	})
-};
+  $scope.changeCategory = function(id, category, event) {
+    console.log("Changing category ", id, category);
+    $http.get('tapi', {
+      params: {
+        mode: "change_cat",
+        value: id,
+        value2: category,
+        output: "json",
+        apikey: "$session"
+      }
+    }).success(function(data, status, headers, config) {})
+  };
 
-$scope.pause = function(id, isPaused, event)
-{
-	var actionType = isPaused ? "resume" : "pause";
-	console.log("Pausing/Resuming queue item", id, actionType);
-	$http.get('tapi', {params: { mode:"queue", name:actionType, value:id, output:"json", apikey: "$session" }}).success(function(data, status, headers, config)
-	{
-	})
-};
-	pollQueue();
+  $scope.pause = function(id, isPaused, event) {
+    var actionType = isPaused ? "resume" : "pause";
+    console.log("Pausing/Resuming queue item", id, actionType);
+    $http.get('tapi', {
+      params: {
+        mode: "queue",
+        name: actionType,
+        value: id,
+        output: "json",
+        apikey: "$session"
+      }
+    }).success(function(data, status, headers, config) {})
+  };
+  pollQueue();
 
-function pollQueue(){
-	queueRefresh.poll().then(function(data)
-	{
-		$scope.queueData = data;
-    $scope.data = data.queue;
-		$timeout(pollQueue, $scope.refresh*1000);
-	})
-}
-}
-);
-app.factory('queueRefresh', function ($http, $timeout) {
-    $http.defaults.cache = false;
-    var poller = function () {
-        return $http.get('tapi', {params: { start:0, limit: 10, mode:"queue", output:"json", apikey: "$session" }}).then(function (responseData) {
-            return responseData;
-        });
-    };
+  function pollQueue() {
+    queueRefresh.poll().then(function(data) {
+      $scope.queueData = data;
+      $scope.data = data.queue;
+      $timeout(pollQueue, $scope.refresh * 1000);
+    })
+  }
+});
+app.factory('queueRefresh', function($http, $timeout) {
+  $http.defaults.cache = false;
+  var poller = function() {
+    return $http.get('tapi', {
+      params: {
+        start: 0,
+        limit: 10,
+        mode: "queue",
+        output: "json",
+        apikey: "$session"
+      }
+    }).then(function(responseData) {
+      return responseData;
+    });
+  };
 
-    return {
-        poll: poller
-    }
+  return {
+    poll: poller
+  }
 });
