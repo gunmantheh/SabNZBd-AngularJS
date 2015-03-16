aa2678428b7729e2b5e4cc42dd5e95377a3d0332
GunmanTheH <4093289+gunmantheh@users.noreply.github.com>
2015-03-16 21:30:32 +0100

    initial support for drag and drop sorting

--- templates/static/main.js
+++ templates/static/main.js
@@ -1,7 +1,9 @@
-var app = angular.module("sabnzbdApp", ['ui.bootstrap', 'ngAnimate']);
-app.controller("PostsCtrl", function($scope, $http, $interval) {
-  $scope.refresh = 5;
-
+var app = angular.module("sabnzbdApp", ['ui.bootstrap', 'ngAnimate', 'ui.sortable']);
+app.controller("PostsCtrl", function($scope, $http, $timeout) {
+  $scope.refresh = 1;
+  $scope.maxSize = 5;
+  $scope.noOfRows = 10;
+  $scope.refreshing = true;
   $scope.deleteNZB = function(id, event) {
     console.log("Removing queue item", id);
     $http.get('tapi', {
@@ -28,6 +30,19 @@ app.controller("PostsCtrl", function($scope, $http, $interval) {
     }).success(function(data, status, headers, config) {});
   };
 
+  $scope.speedLimit = function(speed, event) {
+    console.log("Changing speedlimit ", speed);
+    $http.get('tapi', {
+      params: {
+        mode: "config",
+        name: "speedlimit",
+        value: speed,
+        output: "json",
+        apikey: $scope.sessionkey
+      }
+    }).success(function(data, status, headers, config) {});
+  };
+
   $scope.pause = function(id, isPaused, event) {
     var actionType = isPaused ? "resume" : "pause";
     console.log("Pausing/Resuming queue item", id, actionType);
@@ -41,19 +56,64 @@ app.controller("PostsCtrl", function($scope, $http, $interval) {
       }
     }).success(function(data, status, headers, config) {});
   };
-  pollQueue($scope.sessionkey || "KeyWasNotPassedDown");
 
-  function getQueue() {
+  $scope.counter = 0;
+  $scope.getQueue = function() {
+    console.log('Called getQueue, using API: ' + $scope.sessionkey);
     $http.get('tapi', {
       params: {
-        start: 0,
-        limit: 10,
+        start: $scope.currentPage == 1 ? 0 : $scope.currentPage * $scope.noOfRows,
+        limit: $scope.noOfRows,
         mode: "queue",
         output: "json",
-        apikey: sessionkey
+        apikey: $scope.sessionkey
       }
     }).success(function(data, status, headers, config) {
-      $session.queueData = data;
+      $scope.queue = data.queue;
+      //$scope.counter++;
+      //console.log('response ' + $scope.counter);
+      $scope.queuetimeout = $timeout($scope.getQueue, $scope.refresh * 1000);
     });
-  }
+  };
+  $scope.queuetimeout = $timeout($scope.getQueue, $scope.refresh * 1000);
+
+  $scope.getQueueChange = function(status) {
+    if (status) {
+      $scope.queuetimeout = $timeout($scope.getQueue, $scope.refresh * 1000);
+    } else {
+      $timeout.cancel($scope.queuetimeout);
+    }
+    $scope.refreshing = status;
+
+  };
+
+  var fixHelper = function(e, ui) {
+    ui.children().each(function() {
+      $(this).width($(this).width());
+    });
+    return ui;
+  };
+
+  $scope.sortableOptions = {
+    helper: fixHelper,
+    connectWith: ".list > tbody",
+    items: '.repeat-item',
+    stop: function(e, ui) {
+      // var logEntry = tmpList.map(function(i){
+      //   return i.value;
+      // }).join(', ');
+      var fromIndex = ui.item.sortable.index;
+      var toIndex = ui.item.sortable.dropindex;
+      console.log('moved', ui.item.scope().slot.nzo_id, ui.item);
+    },
+    update: function(e, ui) {
+      var dropTarget = ui.item.sortable.droptarget;
+      var actualTarget = dropTarget[0].parentElement;
+      if ($(actualTarget).hasClass("queueTable")) {
+        return true;
+      }
+      ui.item.sortable.cancel();
+      return false;
+    },
+  };
 });
